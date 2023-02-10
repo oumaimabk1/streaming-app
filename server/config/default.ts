878683a3a466cee39df90440248bfb23deb53f29
Baccouch oumaima <baccouchoumaima35@gmail.com>
2023-02-10 12:04:08 +0100

    first commit

---
+++ server/config/default.ts
@@ -0,0 +1,5 @@
+export default {
+    port: 1337,
+    host: "localhost",
+    dbUri: "mongodb+srv://user:g09UU8Z2B8CYqlf7@cluster0.qkbvo.mongodb.net/?retryWrites=true&w=majority/streamindApp"
+  }
---
+++ server/src/app.ts
@@ -0,0 +1,30 @@
+// src/app.ts
+
+import express from "express";
+import config from "config";
+import mongoose from "mongoose";
+import routes from "./routes/index";
+
+
+const port = config.get("port") as number;
+const host = config.get("host") as string;
+const url = config.get("dbUri") as string;
+const app = express();
+
+// Parses incoming requests with JSON payloads
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose
+    .connect(
+        url
+    )
+    .then(() => {
+        app.listen(port, host, () => {
+            console.log(`Server listing at http://${host}:${port}`);
+            routes(app);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
