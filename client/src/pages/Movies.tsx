a58f4a1d5f418e5c81edbf685ce8d69b048dc0e8
Baccouch oumaima <baccouchoumaima35@gmail.com>
2023-02-17 15:35:11 +0100

    fetch data

--- client/src/pages/Movies.tsx
+++ client/src/pages/Movies.tsx
@@ -1,37 +1,31 @@
-import {Flex} from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import ProductAddToCart from "../components/MovieCard";
 import MovieCard from "../components/MovieCard";
 import { fetchMOVIEData } from "../redux/actions/moviesActions";
 import { movieTypes } from "../redux/types";
 
 // Step 3: Create a React component that will connect to the store and render the API data
 const MovieComponent = () => {
-    const movieData = useSelector((state: any) => state.moviesData.movieData);
-    console.log(movieData)
-    return (
-        <Flex
-            flexDirection="column"
-            width="100wh"
-            height="100vh"
-            backgroundColor="gray.200"
-            justifyContent="center"
-            alignItems="center"
-        >
-           {movieData.map((el:movieTypes,index:number)=>{
-            <MovieCard key={index} data={el} />
-           })}
-        </Flex>
-    );
-  };
-  
-  // Step 4: Dispatch the API request action when the component mounts
-  const Movies = () => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-      dispatch(fetchMOVIEData(1));
-    }, []);
-    return <MovieComponent />;
-  };
-  
-  export default Movies;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMOVIEData(1));
+  }, []);
+  const movieData = useSelector((state: any) => state.moviesData.movieData);
+  console.log(movieData)
+  return (
+    <SimpleGrid columns={[1, 2, 3]} spacing="1">
+      {movieData && movieData.map((el: movieTypes, index: number) => {
+        <ProductAddToCart  key={index}/>
+      })}
+    </SimpleGrid>
+  );
+};
+
+// Step 4: Dispatch the API request action when the component mounts
+const Movies = () => {
+  return <MovieComponent />;
+};
+
+export default Movies;
--- client/src/redux/actions/moviesActions.ts
+++ client/src/redux/actions/moviesActions.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // Step 1: Define an action that will make the API request
 export const fetchMOVIEData = (page:number):any  => {
+  console.log(page)
   return async (dispatch: any) => {
     const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=6cc1df6659017d51dec12febc2690279&language=en-US&page=${page}`);
-    dispatch({ type: 'MOVIE_DATA_RECEIVED', payload: response.data });
+    dispatch({ type: 'MOVIE_DATA_RECEIVED', payload: response.data.results });
   };
 };
