888b72b671af1fd4c9947cab3ffad1690f3d3877
Nico-Capy <nicolacorradini97@yahoo.com>
2023-02-21 12:13:18 +0100

    Landing

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,10 @@ import { Text } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import Button from "../components/Button/Button";
+import jurassicpark from "../images/jurassicpark.jpg";
+import office from "../images/office.jpg";
+import starwars from "../images/starwars.jpg";
+import streamy from "../images/streamy.svg";
 
 const Home = () => {
   return (
@@ -24,14 +28,20 @@ const Home = () => {
         justifyContent="center"
         padding="1rem"
       >
+        
+        <Image src="streamy.svg" alt="Streamy logo" h="2rem" />
+
         <Text>Welcome to</Text>
         <Heading>Streamy</Heading>
         <Text>Home of ALL the movies</Text>
-      </Flex>
-      <Flex flexDirection="row">
-        <Image src="starwars.jpg" alt="Star Wars" />
-        <Image src="jurassicpark.jpg" alt="Jurassic Park" />
-        <Image src="office.jpg" alt="The Office" />
+        <br /><br />
+        <Text>Welcome to Streamy, where you can discover and enjoy a wide range of movies and TV shows from around the world. With our easy-to-use interface and extensive library, you can dive into your favorite genres or explore new ones. From action-packed blockbusters to heartwarming dramas, we have something for everyone. And with new titles added regularly, you'll always have something new to watch. Sign up now and start streaming!</Text>
+        
+        <Flex justifyContent="center" marginTop="1rem">
+          <Image src="starwars.jpg" alt="Star Wars" />
+          <Image src="jurassicpark.jpg" alt="Jurassic Park" />
+          <Image src="office.jpg" alt="The Office" />
+        </Flex>
       </Flex>
     </Flex>
   );
