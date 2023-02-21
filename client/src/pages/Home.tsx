d629f7fd9d5bf31995b4a3d64a0b2164920047a7
Nico-Capy <nicolacorradini97@yahoo.com>
2023-02-21 15:44:41 +0100

    Légères modifications

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -19,7 +19,6 @@ const Home = () => {
       backgroundColor="gray.200"
       alignItems="top right"
       justifyContent="flex-end"
-      padding="1rem"
     >
       <Button btnText="Log in" />
       <Button btnText="Sign up" />
@@ -31,14 +30,15 @@ const Home = () => {
         padding="1rem"
       >
         
-        <Text mt="6">Welcome to</Text>
+        <Text mb="0">Welcome to</Text>
 
         <Image src={streamy} alt="Streamy logo" h="8rem" m="6"/>
 
         <Heading>Streamy</Heading>
+        <br /><br />
         <Text>Home of ALL the movies</Text>
         <br /><br />
-        <Text>Welcome to Streamy, where you can discover and enjoy a wide range of movies and TV shows from around the world. With our easy-to-use interface and extensive library, you can dive into your favorite genres or explore new ones. From action-packed blockbusters to heartwarming dramas, we have something for everyone. And with new titles added regularly, you'll always have something new to watch. Sign up now and start streaming!</Text>
+        <Text>Welcome to Streamy, where you can discover and enjoy all the movies and TV shows (and we really mean ALL of them) from around the world. With our easy-to-use interface and extensive library, you can dive into your favorite genres or explore new ones. From action-packed blockbusters to heartwarming dramas, we have something for everyone. And with new titles added regularly, you'll always have something new to watch. Sign up now and start streaming!</Text>
         
         <Flex justifyContent="center" marginTop="1rem">
           <Image src={starwars} alt="Star Wars" h="16rem" m="6"/>
