50b3dcd2c18f1f346d067c23f8e0bde45caf2197
Nico-Capy <nicolacorradini97@yahoo.com>
2023-02-21 13:32:59 +0100

    Almost done

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,6 +8,7 @@ import office from "../images/office.jpg";
 import starwars from "../images/starwars.jpg";
 import streamy from "../images/streamy.svg";
 
+
 const Home = () => {
   return (
     <Flex
@@ -29,18 +30,19 @@ const Home = () => {
         padding="1rem"
       >
         
-        <Image src="streamy.svg" alt="Streamy logo" h="2rem" />
-
         <Text>Welcome to</Text>
+
+        <Image src={streamy} alt="Streamy logo" h="8rem" m="6"/>
+
         <Heading>Streamy</Heading>
         <Text>Home of ALL the movies</Text>
         <br /><br />
         <Text>Welcome to Streamy, where you can discover and enjoy a wide range of movies and TV shows from around the world. With our easy-to-use interface and extensive library, you can dive into your favorite genres or explore new ones. From action-packed blockbusters to heartwarming dramas, we have something for everyone. And with new titles added regularly, you'll always have something new to watch. Sign up now and start streaming!</Text>
         
         <Flex justifyContent="center" marginTop="1rem">
-          <Image src="starwars.jpg" alt="Star Wars" />
-          <Image src="jurassicpark.jpg" alt="Jurassic Park" />
-          <Image src="office.jpg" alt="The Office" />
+          <Image src={starwars} alt="Star Wars" h="16rem" m="6"/>
+          <Image src={jurassicpark} alt="Jurassic Park" h="16rem" m="6"/>
+          <Image src={office} alt="The Office" h="16rem" m="6"/>
         </Flex>
       </Flex>
     </Flex>
