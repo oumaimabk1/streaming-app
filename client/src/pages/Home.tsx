4b9f2024678f9986dea9cd124b707c38dbdb7d2b
Nico-Capy <nicolacorradini97@yahoo.com>
2023-02-23 12:13:01 +0100

    Mieux adapté pour ordinateur

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -9,6 +9,12 @@ import { Center, Square, Circle } from '@chakra-ui/react';
 import { Stack, HStack, VStack } from '@chakra-ui/react';
 import Footer from "../Footer/footer";
 
+const breakpoints = {
+  sm: '30em',
+  md: '48em',
+  lg: '62em',
+  xl: '80em',
+}
 
 const Home = () => {
   return (
@@ -19,35 +25,35 @@ const Home = () => {
       bgColor="#1B202B"
       margin="0"
     >
-      <Heading textAlign="left" size="3xl" margin="6" color="white">
+      <Heading textAlign="left" size="3xl" margin={{ base: '6', md: '9', lg: '8', xl: '8'}} color="white">
         Welcome to Streamy, where there's a movie for every mood
       </Heading>
       <Text
         textAlign="left"
         color="white"
-        margin="6"
+        margin={{ base: '6', md: '9', lg: '10', xl: '10'}}
         fontSize="sm"
         justifyItems="center"
       >
-        Discover and enjoy endless movies and TV shows on Streamy! No need to have a subscription to EVERY streaming platform to have a decent selection, we have ALL the movies and shows. From blockbusters to dramas, we have something for every genre lover. Sign up now to start streaming.
+        Discover and enjoy endless movies and TV shows on Streamy! From blockbusters to dramas, we have something for every genre lover. Sign up now to start streaming.
       </Text>
 
       <Flex
         flexDirection="column"
         alignItems="center"
       >
-        <Button borderRadius="0" marginTop="6" marginBottom="3" width="93%" colorScheme='teal'>Log in</Button>
-        <Button borderRadius="0" marginTop="6" marginBottom="3" width="93%" color="#009594" colorScheme='black'>Sign up</Button>
+        <Button borderRadius="0" marginTop={{ base: '3', md: '9', lg: '9', xl: '9'}} marginBottom={{ base: '3', md: '6', lg: '9', xl: '10'}} width="93%" colorScheme='teal'>Log in</Button>
+        <Button borderRadius="0" marginTop={{ base: '3', md: '6', lg: '9', xl: '9'}} marginBottom={{ base: '3', md: '9', lg: '9', xl: '10'}} width="93%" color="#009594" colorScheme='black'>Sign up</Button>
       </Flex>
 
     <Center>
-     <VStack margin="6">
+     <VStack margin={{ base: '2', md: '6', lg: '9', xl: '9'}}>
       <HStack>
-          <Heading size="2xl" textAlign="center" color="white" marginTop="3">4206</Heading>
-          <Heading size="2xl" textAlign="center" color="teal" marginTop="3">   +</Heading>
+          <Heading size="2xl" textAlign="center" color="white" marginTop={{ base: '1', md: '5', lg: '9', xl: '10'}}>4206</Heading>
+          <Heading size="2xl" textAlign="center" color="teal" marginTop={{ base: '1', md: '5', lg: '9', xl: '10'}}>   +</Heading>
         </HStack>
           <Text
-            fontSize="xs"
+            fontSize={{ base: 'xs', md: 'sm', lg: 'sm', xl: 'sm'}}
             textAlign="left"
             color="white"
           >
@@ -57,11 +63,11 @@ const Home = () => {
 
       <VStack margin="6">
       <HStack>
-          <Heading size="2xl" textAlign="center" color="white" marginTop="6">1615</Heading>
-          <Heading size="2xl" textAlign="center" color="teal" marginTop="6">   +</Heading>
+          <Heading size="2xl" textAlign="center" color="white" marginTop={{ base: '1', md: '5', lg: '9', xl: '10'}}>1615</Heading>
+          <Heading size="2xl" textAlign="center" color="teal" marginTop={{ base: '1', md: '5', lg: '9', xl: '10'}}>   +</Heading>
         </HStack>
           <Text
-            fontSize="xs"
+            fontSize={{ base: 'xs', md: 'sm', lg: 'sm', xl: 'sm'}}
             textAlign="center"
             color="white"
           >
@@ -70,7 +76,7 @@ const Home = () => {
       </VStack>
     </Center>
       
-      <Box color="white" position="absolute" bottom="0" width="100%" marginTop="6">
+      <Box color="white" position="absolute" bottom="0" width="100%" marginTop="3">
         <Footer companyName="Streamy" creators={["Oumaima", "Zineb", "Nico"]} />
       </Box>
     </Flex>
